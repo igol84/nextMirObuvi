c4ae43d4ec8c759f86a4ff3169a949fa8d2164cf
igol84 <igolub84@gmail.com>
2023-08-14 17:17:35 +0300

    Brands refactoring

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Home from "@/app/[lang]/Home";
 import {BrandProps} from "@/components/Brands/types";
+import {BrandSchema} from "@/schemas/brands";
 
 async function getData(){
   const result = await fetch('https://mirobuvi.com.ua/xml_ftp/brands.json')
@@ -11,7 +12,7 @@ async function getData(){
 }
 
 const Page = async () => {
-  const brandsData = await getData()
+  const brandsData: BrandSchema[] = await getData()
   const brands: BrandProps[] = brandsData.map(brand=>({
     brandId: brand.id, brandName: brand.name, url: brand.url
   }))
