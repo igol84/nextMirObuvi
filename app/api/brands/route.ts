ff699a6babc6960be52b02c20442806796b95fa0
igol84 <igolub84@gmail.com>
2023-08-18 17:57:13 +0300

    Brand Page

--- app/api/brands/route.ts
+++ app/api/brands/route.ts
@@ -1,8 +1,14 @@
 import {NextResponse} from 'next/server'
-
+import {Data} from "@/schemas/brands";
 
 export async function GET() {
+  const res = await fetch(`https://mirobuvi.com.ua/xml_ftp/data.json`, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+  const product: Data = await res.json()
+  const brands = product.brands
 
-
-  return NextResponse.json({hello: 'sd'})
+  return NextResponse.json(brands)
 }
