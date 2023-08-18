4d32761282f47d98d3b111e6eb4b8136c0cbfb1a
igol84 <igolub84@gmail.com>
2023-08-18 18:10:08 +0300

    Brand Page

--- app/[lang]/brands/page.tsx
+++ app/[lang]/brands/page.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import Brands from "@/components/Brands";
-import {BrandSchema, Data} from "@/schemas/brands";
+import {BrandSchema} from "@/schemas/brands";
 import {BrandProps} from "@/components/Brands/types";
 
 export const revalidate = 3600
 
 export async function getBrandsData(): Promise<BrandSchema[]> {
-  const res = await fetch(`https://mirobuvi.com.ua/xml_ftp/data.json`, {
+  const res = await fetch(`https://mir-obuvi.vercel.app/api/brands`, {
     headers: {
       'Content-Type': 'application/json',
     },
   })
-  const product: Data = await res.json()
-  return product.brands
+  return await res.json()
 }
 
 
