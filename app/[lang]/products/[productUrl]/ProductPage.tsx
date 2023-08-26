d6159e40ad24e01cdf53619160bc1c52c2cc146b
igol84 <igolub84@gmail.com>
2023-08-26 19:09:50 +0300

    products generate

--- app/[lang]/products/[productUrl]/ProductPage.tsx
+++ app/[lang]/products/[productUrl]/ProductPage.tsx
@@ -3,7 +3,7 @@ import {ProductType} from "@/components/product/types";
 import {productFactory} from "@/components/product/ProductFactory";
 import React from "react";
 import {Box, Flex, Heading} from "@chakra-ui/react";
-import Gallery from "@/components/product/Galarey";
+
 
 
 type Props = {
@@ -16,7 +16,7 @@ const ProductPage = ({productData}: Props) => {
     <>
       <Flex flexDirection={{base: 'column', lg: 'row'}} pb={8}>
         <Box w={{base: '100%', lg: '60%'}}>
-          <Gallery images={productData.images}/>
+
         </Box>
         <Box w={{base: '100%', lg: '38%'}}>
           {product}
