91308c7f5bc840732ed1e1eaa85d58c0bb10a419
igol84 <igolub84@gmail.com>
2023-09-18 16:46:33 +0300

    is Loading

--- app/[lang]/Home.tsx
+++ app/[lang]/Home.tsx
@@ -4,11 +4,14 @@ import {Box, Flex} from "@chakra-ui/react";
 import Carousel from "@/components/Carousel";
 import Brands from "@/components/Brands";
 import {useBrands} from "@/app/[lang]/hooks";
+import LoadingBrands from "@/app/[lang]/loading";
 
 
 const Home = () => {
-  const {brands} = useBrands()
+  const {brands, isError, isLoading} = useBrands()
   const requiredData = brands && brands.map(brand => ({brandId: brand.id, brandName: brand.name, url: brand.url}))
+  if (isLoading) return <LoadingBrands/>
+  if (isError) return <p>Error</p>
   return (
     <>
       <Flex justifyContent='center' alignItems='center' pb={8}>
---
+++ app/[lang]/loading.tsx
@@ -0,0 +1,9 @@
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+  title: "Brands"
+}
+
+export default function LoadingBrands() {
+  return <h1>Loading...</h1>
+}
