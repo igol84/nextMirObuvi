01e0f4ec81b689b00cf6413a84fa7c9b297939e3
igol84 <igolub84@gmail.com>
2023-09-21 16:34:26 +0300

    fix ssl api

--- app/[lang]/products/[productUrl]/ProductPage.tsx
+++ app/[lang]/products/[productUrl]/ProductPage.tsx
@@ -9,14 +9,15 @@ import Gallery from "@/components/product/Galarey";
 type Props = {
   productData: ProductType
 }
-
+const IMAGES = ['03', '13', '23', '33']
 const ProductPage = ({productData}: Props) => {
   const product = productFactory(productData)
+  const images = productData.images.filter(url=> IMAGES.some(name => url.includes(name)))
   return (
     <>
       <Flex flexDirection={{base: 'column', lg: 'row'}} pb={8}>
         <Box w={{base: '100%', lg: '60%'}}>
-          <Gallery images={productData.images}/>
+          <Gallery images={images}/>
         </Box>
         <Box w={{base: '100%', lg: '38%'}}>
           {product}
