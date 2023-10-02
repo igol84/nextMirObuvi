a63635cfb161bde13249544c15d0b77f0fade77f
igol84 <igolub84@gmail.com>
2023-10-02 21:51:36 +0300

    fix

--- components/product/action.ts
+++ components/product/action.ts
@@ -18,7 +18,7 @@ export async function incrementProductQuantity(productId: string, size?: number 
     await  prisma.cartItem.create({
       data: {
         cartId: cart.id,
-        size: size ? size : null,
+        size: size ? size : 0,
         productId,
         quantity: 1
       }
