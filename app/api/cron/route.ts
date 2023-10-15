41e33499d5bddb8ecf0db47db99ace30aaaec43b
igol84 <igolub84@gmail.com>
2023-10-15 19:53:20 +0300

    cron old clear carts

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,11 +1,12 @@
 import 'server-only'
+
 import {NextResponse} from 'next/server'
 import {clearOldCart} from "@/lib/db/cart";
-
+export const dynamic = 'force-dynamic'
 export async function GET() {
   const carts = await clearOldCart()
   return NextResponse.json(
-    { message: {deletedCarts: carts} },
+    { message: {deletedCarts: carts, date: new Date().toJSON()} },
     { status: 200 },
   );
 }
