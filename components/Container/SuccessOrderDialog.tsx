6e3d27aace2b3895ea6daa871c3d0f55e0a851f6
igol84 <igolub84@gmail.com>
2023-10-24 11:13:53 +0300

    modal dialog order success

--- components/Container/SuccessOrderDialog.tsx
+++ components/Container/SuccessOrderDialog.tsx
@@ -19,16 +19,17 @@ type Props = {
 
 const SuccessOrderDialog = ({initial}: Props) => {
   const d = useDictionaryTranslate("orderForm")
-  const [IsOpen, setIsOpen] = useState(initial)
+  const [isOpen, setIsOpen] = useState(initial)
   const onClose = () => setIsOpen(false)
   return (
-    <Modal closeOnOverlayClick={false} isOpen={IsOpen} onClose={onClose}>
+    <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose}>
       <ModalOverlay/>
       <ModalContent>
         <ModalHeader>{d('dialogSuccessHeader')}</ModalHeader>
         <ModalCloseButton/>
         <ModalBody>
-          <Text fontWeight='bold' mb='1rem' dangerouslySetInnerHTML={{__html: d('dialogSuccessBody')}}></Text>
+          <Text fontWeight='bold' mb='1rem'>{d('dialogSuccessBody1')}</Text>
+          <Text fontWeight='bold' mb='1rem'>{d('dialogSuccessBody2')}</Text>
         </ModalBody>
         <ModalFooter>
           <Button colorScheme='blue' mr={3} onClick={onClose}>
