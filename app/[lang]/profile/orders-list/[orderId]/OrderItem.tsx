9c4f9ed4920d1746acde9a366553646e4ff578cc
igol84 <igolub84@gmail.com>
2023-10-26 17:17:37 +0300

    order detail fix

--- app/[lang]/profile/orders-list/[orderId]/OrderItem.tsx
+++ app/[lang]/profile/orders-list/[orderId]/OrderItem.tsx
@@ -1,11 +1,11 @@
 import React, {useContext} from 'react';
-import {Box, Flex, Text} from "@chakra-ui/react";
+import {Box, Flex, Link, Text} from "@chakra-ui/react";
 import ChakraNextImage from "@/components/base/ChakraNextImage";
 import NextImage from "next/image";
 import {LangContext} from "@/locale/LangProvider";
-import {useRouter} from "next/navigation";
 import {IOrderItem} from "@/app/[lang]/profile/orders-list/[orderId]/types";
 import {useDictionaryTranslate} from "@/dictionaries/hooks";
+import NextLink from "next/link";
 
 interface Props {
   orderItem: IOrderItem
@@ -15,21 +15,21 @@ const OrderItem = ({orderItem}: Props) => {
   const d = useDictionaryTranslate("orderList")
   const UAHFormat = new Intl.NumberFormat('ru-RU', {style: 'decimal'})
   const lang = useContext(LangContext)
-  const productName = lang === 'en' ? orderItem.productNameEn: orderItem.productNameUa
-  const router = useRouter();
-  const onClick = () => {
-    router.push(`/${lang}/products/${orderItem.url}`, {scroll: false})
-  }
+  const productName = lang === 'en' ? orderItem.productNameEn : orderItem.productNameUa
+
   const size = orderItem.size ? ' ' + orderItem.size : ''
   return (
     <Flex p={4} gap={8} w='full' direction='row' sx={{borderWidth: '1px 0 0 0'}}>
-      <Box  onClick={onClick} sx={{cursor: 'pointer'}}>
+      <Link
+        as={NextLink} href={`/${lang}/products/${orderItem.url}`}
+        _hover={{color: 'hoverLinkTextColor'}}
+      >
         <ChakraNextImage
           shadow='base' borderRadius={[30, 15]} as={NextImage}
           width={249} height={249} alt={productName}
           src={`https://mirobuvi.com.ua/ftp_products/${orderItem.imgUrl}/02.jpg`}
         />
-      </Box>
+      </Link>
       <Flex direction='column'>
         <Box>{productName}{size}</Box>
         <Box>{UAHFormat.format(orderItem.price)}{d('pricePrefix')} × {orderItem.quantity} = {' '}
