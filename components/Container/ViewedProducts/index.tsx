5dad5955a28d26199d87a97ea729f2bfc9d2b5d7
igol84 <igolub84@gmail.com>
2023-11-15 14:09:36 +0200

    viewed Products fix

--- components/Container/ViewedProducts/index.tsx
+++ components/Container/ViewedProducts/index.tsx
@@ -25,7 +25,7 @@ const ViewedProducts = ({viewedProducts}: Props) => {
       <Flex pt={4} w='100%' alignItems='center' gap={2}>
         <IconButton fontSize={['16', '24', '24', '44']} icon={<MdArrowBackIosNew/>} aria-label='prev'
                     className='review-swiper-button-prev'/>
-        <Box width={'94%'}>
+        <Box width={['60%', '70%', '80%', '94%']}>
         <Swiper
           slidesPerView={1}
           breakpoints={{
